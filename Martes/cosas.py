ab4a04cea77cd8419cebb87d6c41a0e66102cf9c
Armando Olmos <111935065+JArmandoOlmosR@users.noreply.github.com>
2023-06-13 14:34:34 -0600

    Add files via upload

---
+++ Martes/cosas.py
@@ -0,0 +1,9 @@
+
+class Alumno:
+
+    facultad = "FES Aragon"
+
+    def __init__(self, nom, ed, carr): #declara el constructor de la clase
+        self.__nombre = nom
+        self.__edad = ed
+        self.__carrera = carr
