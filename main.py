430107ecf0dc64552860324634c85f664a51aa15
Armando Olmos <111935065+JArmandoOlmosR@users.noreply.github.com>
2023-06-13 14:30:00 -0600

    MartesPOO

---
+++ main.py
@@ -0,0 +1,14 @@
+
+
+#import aritmetica
+#import aritmetica as ari
+
+from aritmetica import suma,resta
+
+def main():
+    resultado = suma(3,None)
+    print(resultado)
+    print(resta(b=5, a=3))
+
+
+main()
